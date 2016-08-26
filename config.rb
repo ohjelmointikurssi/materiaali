e9853676ac3c0aa67f2772b7d0b676a517375ac5
Henrik Nygren <nygrenh@gmail.com>
2016-08-26 15:42:55 +0300

    Move content to partials

--- config.rb
+++ config.rb
@@ -8,6 +8,8 @@
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page '/paste.html', layout: false
+
 
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
